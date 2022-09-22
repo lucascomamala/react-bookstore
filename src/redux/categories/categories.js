c3311c910b3cd71121913c57ad63bae63283db02
Lucas Comamala <lucascomamala@gmail.com>
2022-09-22 12:57:08 -0600

    create categories redux file

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,14 @@
+const CHECK = 'bookstore/categories/CHECK';
+
+export const checkStatusBook = () => ({ type: CHECK });
+
+const categoriesReducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK:
+      return 'Under construction';
+    default:
+      return state;
+  }
+}
+
+export default categoriesReducer;
