0905c4926bd787fb840571e444f4da493029f751
Lucas Comamala <lucascomamala@gmail.com>
2022-09-23 11:54:05 -0600

    Fix configurestore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,11 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers({
-  books: booksReducer,
-  categories: categoriesReducer,
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
-
-const store = configureStore(rootReducer);
-
 export default store;
