c984d67d55bb8fc5d25873c3045e2ca7d18032b5
Lucas Comamala <lucascomamala@gmail.com>
2022-09-23 12:23:36 -0600

    Add functionality for Check Status btn

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatusBook } from '../../redux/categories/categories';
 
-const Categories = () => <button type="button">Check status</button>;
+const Categories = () => {
+  const dispatch = useDispatch();
+  const handleStatus = () => {
+    dispatch(checkStatusBook());
+  };
+  const status = useSelector((state) => state.categories);
+
+  return (
+    <>
+      <button type="button" onClick={handleStatus}>Check status</button>
+      <h2>{status}</h2>
+    </>
+  );
+};
 
 export default Categories;
